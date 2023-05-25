73205c7aa781450275b27e81719d684838b8088a
Tysuiku <timothydong41@gmail.com>
2023-05-25 10:33:52 -0400

    add loading screen for mainpage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,29 @@
+import React, { useState, useEffect } from "react";
 import Introbox from "./IntroBox";
+import LoadImage from "./hololiveLoad.png";
+import "./HomePage.css";
 
 const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadAllComponents = async () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    };
+
+    loadAllComponents();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <img id="LoadScreen" src={LoadImage} />
+      </div>
+    );
+  }
+
   return <Introbox />;
 };
 
