bfc744fbe7c2fafeee9e6fb323e65e45e19e9f46
Tysuiku <timothydong41@gmail.com>
2023-05-28 13:08:30 -0400

    render debug

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,19 +3,13 @@
 # exit on error
 set -o errexit
 
-# Define where to install the gems
-export GEM_HOME=$HOME/.gem
-export GEM_PATH=$GEM_HOME
-
-# Print paths for debugging
 echo "Home directory: $HOME"
 echo "Gem home: $GEM_HOME"
 echo "Gem path: $GEM_PATH"
 
+export PATH="$GEM_HOME/bin:$PATH"
+
 npm run build
-
-# Install gems locally
-bundle install --path $GEM_HOME
-
+bundle install
 rails db:migrate
-rails db:seed # if needed
+rails db:seed #if needed
