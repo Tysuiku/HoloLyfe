f11f0bca2a085c6a0c531d1a234fabeff87329b7
Tysuiku <timothydong41@gmail.com>
2023-05-28 14:03:35 -0400

    reverted changes bug continues

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-# Exit on error
+# exit on error
 set -o errexit
 
-# Define a new directory to install gems
-export GEM_HOME="$HOME/gems"
-export GEM_PATH="$GEM_HOME"
-
-export PATH="$GEM_HOME/bin:$PATH"
-
-echo "Home directory: $HOME"
-echo "Gem home: $GEM_HOME"
-echo "Gem path: $GEM_PATH"
-
 npm run build
-
-# Install the latest bundler
-gem install bundler -v 2.3.26
-
-# Use bundler to install gems
-bundle config set --local path "$GEM_HOME"
 bundle install
-
-# Use bundler to run rails commands
-bundle exec rails db:migrate
-bundle exec rails db:seed # if needed
+rails db:migrate
+rails db:seed #if needed
